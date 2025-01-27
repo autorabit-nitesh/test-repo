4bdb834e2ec6b3e7398d358c0e1225afdd4e1631
Nitesh Kumar <nitesh.kumar@autorabit.com>
2025-01-27 14:13:31 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    private static void extractFile(Path zipFile, String fileName, Path outputFile) throws IOException {
+        try (var fileSystem = FileSystems.newFileSystem(zipFile, (ClassLoader) null)) {
+            var fileToExtract = fileSystem.getPath(fileName);
+            Files.copy(fileToExtract, outputFile);
+        }
+    }
+}
