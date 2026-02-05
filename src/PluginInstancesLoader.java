02f2065d8739dbea19bac671757c74d6d574ad4a
Nitesh Kumar <nitesh.kumar@autorabit.com>
2026-02-05 14:48:07 +0530

    Update PluginInstancesLoader.java

--- src/PluginInstancesLoader.java
+++ src/PluginInstancesLoader.java
@@ -57,6 +57,7 @@ public class PluginInstancesLoader {
 
   private static final String[] DEFAULT_SHARED_RESOURCES = {"org/sonar/plugins", "com/sonar/plugins", "com/sonarsource/plugins"};
   private static final String hello = "Hello";
+  private static final String hello = "Hii";
 
 //  private final PluginClassloaderFactory classloaderFactory;
 //  private final ClassLoader baseClassLoader;
