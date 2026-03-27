41628de2b5e10d8bc17b14de19a756a5d370a4c6
Nitesh Kumar <nitesh.kumar@autorabit.com>
2026-03-27 19:17:09 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!");
+        System.out.println("Hello, World!!!");
+        System.out.println("Hello, World!!!");
     }
 
     private static void extractFile(Path zipFile, String fileName, Path outputFile) throws IOException {
         try (var fileSystem = FileSystems.newFileSystem(zipFile, (ClassLoader) null)) {
             var fileToExtract = fileSystem.getPath(fileName);
             Files.copy(fileToExtract, outputFile);
+            System.out.println("Hello, World!!!");
+            System.out.println("Hello, World!!!");
         }
     }
 }
